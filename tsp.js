1b9a7676bbc04030812cc01df521de113306c661
Adam <57196182+adambgdn@users.noreply.github.com>
2023-04-19 21:57:07 +0200

    Add files via upload

---
+++ tsp.js
@@ -0,0 +1,15 @@
+// Add event listeners to the buttons
+document.getElementById("option1").addEventListener("click", function () {
+    window.location.href = "./sa/sa.html";
+});
+
+document.getElementById("option2").addEventListener("click", function () {
+    window.location.href = "./ga/ga.html";
+});
+
+document.getElementById("option3").addEventListener("click", function () {
+    window.location.href = "./bea/bea.html";
+});
+document.getElementById("option4").addEventListener("click", function () {
+    window.location.href = "./dbmea/dbmea.html";
+});
